beeeb2f550440a68d476f6bd4c5791513e63bf8f
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-05-14 02:05:25 -0400

    added background animation on hover

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  // 
+  // useEffect(() =>{
+  //   console.log("hittinh")
+  //     document.getElementsByClassName("Home_bg_slider__1jKsY")[0].style.color='var(--slideTextColor)'
+  //     document.getElementsByClassName("Home_bg_slider__1jKsY")[0].style.transition='background_position=-50% 100%'
+  //     // document.getElementsByClassName("Home_bg_slider__1jKsY")[0].style.background_size='400%'
+  //     console.log("yeah")
+  // })
+
+
   return (
     <div>
       <Head>
@@ -10,7 +22,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-          <p className={styles.mainTitle}>Beyond Capital</p>
+        <div className={styles.bg_slider} >
+            <p className={styles.mainTitle}>Beyond Capital</p>
+        </div>
       </div>
     </div>
   )
